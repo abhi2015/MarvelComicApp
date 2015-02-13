3f83c19d3d6a3fb3b7d74c1f938851daf068e892
Your Full Name <mayankasheoran@Mayankas-MacBook-Pro.local>
2015-02-13 18:44:50 +0530

    added new transitions

--- js/transitions.js
+++ js/transitions.js
@@ -4,3 +4,20 @@ function scroll() {
     console.log(scrollValue);
     document.getElementById("appbar").style.top = (-(scrollValue / 3)).toString() + "px";
 };
+
+function opaque(x)
+{
+
+  
+  x.style.opacity=0.1;
+
+}
+
+function normal(x)
+{
+
+  
+  x.style.opacity=1;
+
+}
+
