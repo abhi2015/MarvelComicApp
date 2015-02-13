8aebd82990f764f53adeed0c36dbbe56a5f438d3
Jaditi <Jaditi.1993@gmail.com>
2015-02-13 16:45:57 +0530

    Final

--- transitions.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = scroll;
-function scroll() {
-    var scrollValue = window.pageYOffset;
-    console.log(scrollValue);
-    document.getElementById("appbar").style.top = (-(scrollValue / 3)).toString() + "px";
-};
