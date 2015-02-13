bc2621a62e41685d517d373bc7a57c6060035f3f
Atul <atulanand94@gmail.com>
2015-02-13 16:36:35 +0530

    data fetch mock

--- js/data_fetch.js
+++ js/data_fetch.js
@@ -1,30 +1,40 @@
 
-var data; 
 function getserieslist(){
 
- var result = fetch('data/serieslist.json');
- 
- var sendtext = result.then(
+ var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
 
-  function(response)
-  {
-    return response.text()
-  }
 
-  ).
- then(
-  function(text) {
-    console.log('got text', text)
-    data = text;
-  }
-  ).then(
-  alert("data loaded")
-  )
+getJSON("data/serieslist.json").then(function(data) {
 
-  .catch(function(ex) {
-    console.log('failed', ex)
-  })
+  var result = document.getElementById("test");
+  result.innerHTML = data.data.results[0].title;
 
-  return sendtext;
+
+
+
+
+
+  
+}, function(status) {
+  alert('Something went wrong.');
+});
+
 
 }
+
+
