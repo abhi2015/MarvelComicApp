589d676dc9f51d47c97077e7f373f88fdc13820e
Jaditi <Jaditi.1993@gmail.com>
2015-02-13 15:48:49 +0530

    Hover

---
+++ js/bigimage.js
@@ -0,0 +1,15 @@
+function big(x) {
+    x.style.height="17%";
+		x.style.width = "17%";
+		x.style.paddingTop="17%";
+		x.style.zIndex=1;
+
+}
+function normal(x)
+{
+	x.style.height="15%";
+	x.style.width="15%";
+	x.style.paddingTop="15%";
+	x.style.zIndex=0;
+
+}
