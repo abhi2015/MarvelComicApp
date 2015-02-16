51967e939fc982e48667265a0b1d9fde3363d557
Atul <atulanand94@gmail.com>
2015-02-16 12:29:51 +0530

    added links

--- js/data_fetch.js
+++ js/data_fetch.js
@@ -16,21 +16,6 @@ var getJSON = function(url) {
 };
 
 
-var jsonfile = getQueryVariable("json");
-
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      alert("got "+pair[1]);
-      return pair[1];
-    }
-  } 
-  alert('Query Variable ' + variable + ' not found');
-}
-
 
 function populateSeriesPage(){
 
