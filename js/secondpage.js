5aafea10219dbde2fb923b604aaed31c385c9321
Atul <atulanand94@gmail.com>
2015-02-16 12:30:55 +0530

    added links from first page

--- js/secondpage.js
+++ js/secondpage.js
@@ -18,6 +18,22 @@ function displayCharacters() {
     };
 
 
+var jsonfile = getQueryVariable("json");
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      alert("got "+pair[1]);
+      return pair[1];
+    }
+  } 
+  alert('Query Variable ' + variable + ' not found');
+}
+
+
     getJSON("data/"+jsonfile).then(function(data) {
 
         var name = document.getElementById("character-name");
