ef069abd2c9e5b20c8c5351cf790d9db2400df6b
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-02-16 13:36:22 +0530

    minor tweaks

--- transitions.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = scroll;
-function scroll() {
-    var scrollValue = document.documentElement.scrollTop;
-    console.log(scrollValue);
-    document.getElementById("appbar").style.top = (-(scrollValue / 3)).toString() + "px";
-};
